4f847ac7dcc446258d7a10dd0d1ea00188436852
michele <michele.giovagnoli1@gmail.com>
2023-03-31 17:17:52 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+
+<template></template>
+<script>
+import axios from 'axios';
+import { store } from './store.js';
+
+export default {
+  //COMPONENTI IMPORTATI
+  components: {
+
+  },
+
+  data() {
+    return {
+      store,
+    }
+  },
+  //FUNZIONI
+  methods: {
+  },
+  //FUNZIONI-AVVIO
+  created() {
+
+  },
+  //FUNZIONI-CAMBIAMENTO
+  computed: {
+
+  },
+}
+</script>
+<style lang="scss">
+@use './styles/general.scss';
+</style>
